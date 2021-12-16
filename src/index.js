161d9f9ae09b53273cf2667976763fb20459ebf4
Anjacodes <anja_schmidt7@posteo.de>
2021-12-16 13:38:46 -0600

    Add Styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 // import _ from 'lodash';
 import './style.css';
+import DummyLogo from '../assets/images/DummyLogo.png';
+import Facebook from '../assets/images/facebook.png';
+import Twitter from '../assets/images/twitter.png';
+import Instagram from '../assets/images/instagram.png';
 
 const btnRefresh = document.querySelector('.btnRefresh');
 const btnForm = document.querySelector('.btnForm');
@@ -7,6 +11,21 @@ const alert = document.querySelector('.alert');
 const table = document.querySelector('.table');
 const nameInput = document.getElementById('name');
 const scoreInput = document.getElementById('score');
+const logo = document.querySelectorAll('.logo');
+const contact = document.querySelector('.contact');
+const mainTitle = document.querySelector('.mainTitle');
+const scoreCont = document.querySelectorAll('.scoreCont');
+const scores = document.querySelector('.scores');
+const contactCont = document.querySelector('.contactCont');
+const socialIcon = document.querySelectorAll('.socialIcon');
+
+window.addEventListener('load', () => {
+  logo[0].setAttribute('src', DummyLogo);
+  logo[1].setAttribute('src', DummyLogo);
+  socialIcon[0].setAttribute('src', Facebook);
+  socialIcon[1].setAttribute('src', Twitter);
+  socialIcon[2].setAttribute('src', Instagram);
+});
 
 btnForm.addEventListener('click', () => {
   fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/1/scores/', {
@@ -21,6 +40,7 @@ btnForm.addEventListener('click', () => {
         await response.json();
         alert.classList.add('green');
         alert.textContent = '✅ Score added successfully!';
+        alert.style.backgroundColor = 'white';
         setTimeout(() => {
           alert.classList.remove('green');
           alert.textContent = '';
@@ -30,6 +50,7 @@ btnForm.addEventListener('click', () => {
       } else {
         alert.classList.add('red');
         alert.textContent = '❌ Failed to add score';
+        alert.style.backgroundColor = 'white';
         throw new Error(`${response.status} Failed Fetch`);
       }
     }).catch((e) => console.error('EXCEPTION: ', e));
@@ -51,4 +72,18 @@ btnRefresh.addEventListener('click', () => {
       });
     }
   })();
+});
+
+contact.addEventListener('click', () => {
+  mainTitle.textContent = 'CONTACT';
+  scoreCont[0].style.display = 'none';
+  scoreCont[1].style.display = 'none';
+  contactCont.style.display = 'block';
+});
+
+scores.addEventListener('click', () => {
+  mainTitle.textContent = 'LEADERBOARD';
+  contactCont.style.display = 'none';
+  scoreCont[0].style.display = 'block';
+  scoreCont[1].style.display = 'block';
 });
